347e8cdfd57d2b7e928f2bce414155b605890bf6
Sangyao42 <wangsangyao@hotmail.com>
2022-10-13 20:47:08 +0200

    memchr modified

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 20:32:53 by sawang            #+#    #+#             */
-/*   Updated: 2022/10/13 20:43:07 by sawang           ###   ########.fr       */
+/*   Updated: 2022/10/13 20:46:48 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@ void	*ft_memchr(const void *s, int c, unsigned long n)
 	unsigned long	i;
 	unsigned char	*sc;
 
+	*sc = s;
 	i = 0;
 	while (s && i < n)
 	{
