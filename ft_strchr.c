6c4f4cb9d5adff429a29de1cdad8ebb7b34dbd6d
Sangyao42 <wangsangyao@hotmail.com>
2022-10-28 17:03:39 +0200

    added type casting when c=0

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 18:18:06 by sawang            #+#    #+#             */
-/*   Updated: 2022/10/24 13:10:01 by sawang           ###   ########.fr       */
+/*   Updated: 2022/10/28 16:53:32 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strchr(const char *s, int c)
 
 	str = (char *)s;
 	s_len = ft_strlen(str);
-	if (c == 0)
+	if ((char) c == 0)
 		return (&str[s_len]);
 	i = 0;
 	while (str[i] != '\0' && i < s_len)
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:08:23 by sawang            #+#    #+#             */
-/*   Updated: 2022/10/18 16:51:48 by sawang           ###   ########.fr       */
+/*   Updated: 2022/10/28 16:53:05 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strrchr(const char *s, int c)
 	s_len = 0;
 	while (str[s_len] != '\0')
 		s_len++;
-	if (c == 0)
+	if ((char) c == 0)
 		return (&str[s_len]);
 	i = 0;
 	while (s[i] != '\0' && i < s_len)
