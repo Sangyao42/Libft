107c07ced0cb4f3f13f21484db14f7a59bc5daa0
Sangyao42 <wangsangyao@hotmail.com>
2022-10-28 17:02:48 +0200

    explicitly added malloc protection

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,21 +6,11 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 21:02:48 by sawang            #+#    #+#             */
-/*   Updated: 2022/10/21 22:21:23 by sawang           ###   ########.fr       */
+/*   Updated: 2022/10/28 16:41:34 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// int	ft_strlen(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i] != '\0')
-// 		i++;
-// 	return (i);
-// }
 
 char	*ft_strdup(const char *s1)
 {
@@ -28,15 +18,14 @@ char	*ft_strdup(const char *s1)
 	char	*dup;
 
 	dup = (char *)malloc(ft_strlen(s1) + 1);
+	if (!dup)
+		return (NULL);
 	i = 0;
-	if (dup)
+	while (s1[i] != '\0')
 	{
-		while (s1[i] != '\0')
-		{
-			dup[i] = s1[i];
-			i++;
-		}
-		dup[i] = '\0';
+		dup[i] = s1[i];
+		i++;
 	}
+	dup[i] = '\0';
 	return (dup);
 }
