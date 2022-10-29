5f1a555bfcc4df5f70ecf80df02bd243244e14dc
Sangyao42 <wangsangyao@hotmail.com>
2022-10-29 21:38:00 +0200

    added 2nd part of bonus functions and updated libft.h and Makefile

--- Makefile
+++ Makefile
@@ -8,13 +8,14 @@ ft_isalnum.c    ft_isprint.c    ft_memmove.c    ft_strlcat.c    ft_strnstr.c    
 ft_strjoin.c    ft_strtrim.c    ft_split.c    ft_itoa.c    ft_strmapi.c    ft_striteri.c\
 ft_putchar_fd.c    ft_putstr_fd.c    ft_putendl_fd.c    ft_putnbr_fd.c
 OBJS = $(SRCS:.c=.o)
-BONUS_SRCS = ft_lstnew.c    ft_lstadd_front.c    ft_lstsize.c    ft_lstlast.c
+BONUS_SRCS = ft_lstnew.c    ft_lstadd_front.c    ft_lstsize.c    ft_lstlast.c    ft_lstadd_back.c\
+ft_lstdelone.c    ft_lstclear.c    ft_lstiter.c    ft_lstmap.c
 BONUS_OBJS = $(BONUS_SRCS:.c=.o)
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	ar rc $(NAME) $(OBJS)
+	ar rcs $(NAME) $(OBJS)
 
 $(OBJS): $(SRCS)
 	$(CC) $(CFLAGS) -c $(SRCS)
@@ -27,8 +28,8 @@ fclean: clean
 
 re: fclean all
 
-bonus: $(BONUS_OBJS)
-	ar rc $(NAME) $(BONUS_OBJS)
+bonus: $(BONUS_OBJS) $(OBJS)
+	ar rcs $(NAME) $(BONUS_OBJS) $(OBJS)
 
 $(BONUS_OBJS): $(BONUS_SRCS)
 	$(CC) $(CFLAGS) -c $(BONUS_SRCS)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 18:20:37 by sawang            #+#    #+#             */
-/*   Updated: 2022/10/28 19:57:35 by sawang           ###   ########.fr       */
+/*   Updated: 2022/10/29 19:34:56 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,11 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
